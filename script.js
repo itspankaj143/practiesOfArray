fdafd018eb19701ebed4131ed1d23247f9997f70
Pankaj Kumar <124787647+itspankaj143@users.noreply.github.com>
2023-04-16 10:20:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+// Switch
+
+// Mini App based on user
+// let user = "admin";
+
+// switch (user) {
+//   case "admin":
+//     console.log("YOu have got full access to the website");
+//     break;
+//   case "mentor":
+//     console.log("YOu have got Course Access");
+//     break;
+//   case "student":
+//     console.log("YOu have got access to purchased Course Access");
+//     break;
+//   default:
+//     console.log("You are Normal User");
+// }
+// Date:
+// let anurag = new Date();
+// // console.log(now.toString());
+// console.log(anurag.getFullYear());
+// console.log(anurag.getDay());
+// console.log(anurag.getMonth());
+// console.log(anurag.getMinutes());
+// console.log(anurag.getHours());
+
+// Array
+
+// const Arr1 = [];
+// console.log(Arr1);
+
+// let names = ["Anurag", "Gupta", "Sudhir", "Suraj", "HiteshSir", "Ankur"];
+// console.log(Arr1);
+// console.log(Arr1[2]); // searching Element Based on String
+// price.push("Aman");
+// console.log(price);
+// console.log(names.length);
+// // console.log(names[names.length - 1]);
+// console.log(names[2]);
+// console.log(names.slice(1, 4));
+// names.splice(1, 3);
+// console.log(names);
+
+// let lastname = ["abc", "def", "jkl", "Kumar"];
+// let middleName = [1, 2, 3, 4, 5];
+// let fullName = names.concat(lastname, middleName);
+// console.log(fullName);
+
+// let numbers = [1, 2, 3, 4, 5, 6, 7];
+// console.log(numbers.copyWithin(2, 5, 6));
+
+// let names = ["Anurag", "Gupta", "Sudhir", "Suraj", "HiteshSir", "Ankur"];
+// console.log(names.includes("HiteshSir", 3)); HOMEWORK
+// console.log(names.indexOf("Ankur"));
+// console.log(Array.isArray(names));
+
+// let numbers = [
+//   1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 21, 3, 5, 1, 43, 5, 7, 2, 4, 7, 8, 3, 2, 1,
+//   45, 78, 3,
+// ];
+// console.log(numbers.lastIndexOf(3));
+
+// let num = [1, 4, 9, 16, 25];
+// console.log(num.map(Math.sqrt));
+
+let num = [1, 35, 98, 16, 25];
+// console.log(num.pop());
+// console.log(num.shift());
+// console.log(num);
+// console.log(num.reverse());
+// console.log(num.sort());
+
+// num.unshift(91, 71);
+// console.log(num);
+
+// let names = "javascript";
+// let namesss = names.split("");
+// console.log(namesss);
+
+let company = "ineuron, facebook, amazon, google";
+let com = company.split(",");
+console.log(com);
